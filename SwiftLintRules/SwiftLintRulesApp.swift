780d179b37952fc5c96ceaa59f277eb524cc1946
dalalbinhamoud <dalalbinhamoud@gmail.com>
2023-10-21 14:55:30 +0300

    consume swiftlint

---
+++ SwiftLintRules/SwiftLintRulesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftLintRulesApp.swift
+//  SwiftLintRules
+//
+//  Created by Dalal Macbook on 21/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftLintRulesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
