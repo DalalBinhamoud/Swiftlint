659d9745c896f0cef06177d6f481afa7ab0cd8c1
dalalbinhamoud <dalalbinhamoud@gmail.com>
2023-10-21 15:01:52 +0300

    rename variable

--- SwiftLintRules/ContentView.swift
+++ SwiftLintRules/ContentView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 struct ContentView: View {
-    var ddcddfejeifjeifjerifjerfiefjddcddfejeifjeifjerifjerfiefjddcddfejeifjeifjerifjerfiefj = "ddd"
+    var verylongVarableNameToBreakSwifdtlintRuleAndCheckTheExistingErrorAndCodeSmell = "temp"
     var body: some View {
         VStack {
             Image(systemName: "globe")
